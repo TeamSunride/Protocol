7c714d3b4395397fd226c9330416b6c40a6994e4
Tom Danvers <32431576+TomD53@users.noreply.github.com>
2022-12-18 01:18:42 +0000

    call spi.beginTransaction every time we do an SPI transaction

--- src/protocol.cpp
+++ src/protocol.cpp
@@ -101,7 +101,7 @@ SPIProtocol::SPIProtocol(byte chipSelect, SPIClass spiChannel, SPISettings setti
 
 uint8_t SPIProtocol::protocol_begin() {
     pinMode(CS, OUTPUT); // Other pins are configured on spi.begin().
-    _spi.beginTransaction(_settings);
+    digitalWrite(CS, HIGH);
     _spi.begin(); // you have to begin the SPI bus before you can use it --_--
     return 0; // 0 for success - not applicable to all protocols
 }
@@ -110,6 +110,7 @@ byte SPIProtocol::read_reg(byte regAddress) {
     regAddress = READ_BYTE | regAddress; // puts read bit into the 8th bit.
     byte inByte = 0;
     digitalWrite(CS, LOW); // pulls CS low, which begins the transfer
+    _spi.beginTransaction(_settings);
     _spi.transfer(regAddress);  // transfers address over MOSI line
     inByte = _spi.transfer(0x00);
     digitalWrite(CS, HIGH); // pulls CS high, which ends the transfer
@@ -121,6 +122,7 @@ void SPIProtocol::read_regs(byte regAddress, byte *outputPointer, uint length) {
     regAddress = READ_BYTE | regAddress; // puts read bit into the 8th bit.
     byte inByte = 0;
     digitalWrite(CS, LOW); // pulls CS low, which begins the transfer
+    _spi.beginTransaction(_settings);
     _spi.transfer(regAddress); // transfer address of desired register
     for (uint i=0; i<length; i++) {
         inByte = _spi.transfer(0x00);  // transfers 0x00 over MOSI line, recieves a byte over MISO line.
@@ -133,6 +135,7 @@ void SPIProtocol::read_regs(byte regAddress, byte *outputPointer, uint length) {
 uint8_t SPIProtocol::write_reg(byte regAddress, byte data) {
     regAddress = WRITE_BYTE | regAddress; //
     digitalWrite(CS, LOW); // pulls CS low, which begins the transfer
+    _spi.beginTransaction(_settings);
     _spi.transfer(regAddress);
     _spi.transfer(data);
     digitalWrite(CS, HIGH); // pulls CS high, which ends the transfer
@@ -142,6 +145,7 @@ uint8_t SPIProtocol::write_reg(byte regAddress, byte data) {
 uint8_t SPIProtocol::write_regs(byte regAddress, byte *writeBuffer, uint length) {
     regAddress = WRITE_BYTE | regAddress;
     digitalWrite(CS, LOW); // pulls CS low, which begins the transfer
+    _spi.beginTransaction(_settings);
     _spi.transfer(regAddress);
     for (uint i=0; i<length; i++) {
         _spi.transfer(*writeBuffer);
